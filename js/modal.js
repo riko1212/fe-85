6c1ea1c866a28d26304039d1f2a86d6f0f20c878
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-03-23 21:43:49 +0200

    mod-5

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.forEach((el) => {
+    el.addEventListener('click', toggleModal);
+  });
+
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
+  }
+})();
